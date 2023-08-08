2ea164c3f6a2393def00cd969e998158c1dbbe07
Sanan Maarouf <sanan.adnan97@gmail.com>
2023-08-08 23:59:51 +0200

    hover

--- components/landing_hero.js
+++ components/landing_hero.js
@@ -10,7 +10,7 @@ export default function LandingHero() {
             {/* so i gotta style this */}
             <div className="container mx-auto py-20">
                 {/* stuff in the container goes in here */}
-                <div class="flex flex-col p-5 justify-evenly bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 border border-gray-100 my-5 md:flex-row content-center">
+                <div class="flex flex-col p-5 justify-evenly bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 borde border-gray-100 my-5 md:flex-row content-center hover:backdrop-blur-lg">
                     <div class="mt-4 text-center lg:place-self-center">
                         <p class="block mt-1 text-lg font-bold text-white">Hello, i'm Sanan Maarouf</p>
                         <p class="mt-2 text-white">I am a full stack developer</p>
